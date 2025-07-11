6153d4c430a892864aad31ff890cf7bee8d4ea2e
kkc1383 <kangkc09@gmail.com>
2025-07-11 11:23:26 +0900

    prob_2577_b2

---
+++ prob_2577_b2.py
@@ -0,0 +1,14 @@
+import sys
+
+a=int(sys.stdin.readline().strip())
+b=int(sys.stdin.readline().strip())
+c=int(sys.stdin.readline().strip())
+
+result=a*b*c
+my_dict={0:0,1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0}
+arr=list(map(int,str(result)))
+for num in arr:
+    my_dict[num]+=1
+
+for key,value in my_dict.items():
+    print(value)
