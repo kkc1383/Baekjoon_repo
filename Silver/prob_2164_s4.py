94119e9119dd2fa643505512271a602692919d20
kkc1383 <kangkc09@gmail.com>
2025-07-22 13:38:03 +0900

    another solution

--- Silver/prob_2164_s4.py
+++ Silver/prob_2164_s4.py
@@ -18,3 +18,24 @@ for i in range(3,500001):
 print(num_list[n])
 
 # 편법이긴한데 2의 제곱수가 아니면 2씩 늘어나고 2의 제곱수가되면 다시 초기화 되고 다음 제곱수가 나올 때까지 계속 2씩 늘어남
+# 1:1, 2:2, 3:2, 4:4, 5:2, 6:4:, 7:6, 8:8
+
+# queue로 푸는 정석
+# from collections import deque
+# import sys
+
+# n=int(sys.stdin.readline().strip())
+
+# myqueue=deque()
+# for i in range(1,n+1):
+#     myqueue.append(i)
+
+# count=0
+# while len(myqueue)>1:
+#     if count%2==0: # 맨 앞에 수 버리기
+#         myqueue.popleft()
+#     else:
+#         top=myqueue.popleft()
+#         myqueue.append(top)
+#     count+=1
+# print(myqueue[0])
