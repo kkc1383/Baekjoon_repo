81bcf4c2739dd803cfbba704f2b84219acf95785
kkc1383 <kangkc09@gmail.com>
2025-07-30 21:24:25 +0900

    solve prob_2623_g3

---
+++ Gold/Gold 3/prob_2623_g3.py
@@ -0,0 +1,34 @@
+import sys
+from collections import defaultdict, deque
+
+adj_dict=defaultdict(list)
+
+singer_num,case_num=list(map(int,sys.stdin.readline().strip().split()))
+indegree=[0]*(singer_num+1)
+
+for _ in range(case_num):
+    line=list(map(int,sys.stdin.readline().strip().split()))
+    for x in range(1,len(line)-1):
+        if not line[x+1] in adj_dict[line[x]]:    
+            adj_dict[line[x]].append(line[x+1])
+            indegree[line[x+1]]+=1
+
+myqueue=deque()
+for i in range(1,singer_num+1):
+    if indegree[i]==0:
+        myqueue.append(i)
+answer=[]
+while myqueue:
+    top=myqueue.popleft()
+    answer.append(top)
+    for next in adj_dict[top]:
+        indegree[next]-=1
+        if indegree[next]==0:
+            myqueue.append(next)
+
+if len(answer)!=singer_num:
+    print(0)
+else:
+    print(*answer, sep="\n")
+
+# 위상정렬을 사용하는 아주 기본적인 문제
