91f3aec1eb76b6c9c38719a455768138680150fa
kkc1383 <kangkc09@gmail.com>
2025-07-31 15:55:05 +0900

    add comment

--- Gold/Gold 5/prob_9084_g5.py
+++ Gold/Gold 5/prob_9084_g5.py
@@ -7,7 +7,7 @@ for _ in range(test_case):
     coin_list=list(map(int,sys.stdin.readline().strip().split()))    
     target=int(sys.stdin.readline().strip())
     dp=[0]*(target+1)
-    dp[0]=1 # 이거 중요 초기에 아무것도 안 담는 것도 경우임
+    dp[0]=1 # 이거 중요 초기에 아무것도 안 담는 것도 경우임 그래야 다음 코인 자기자신 나올때 1을 더할 수 있음.
     coin_list.sort() # 코인이 작은 것부터 시작해야 하기때문에, 문제 상황상 굳이 안해도 될 거 같음
     
     for coin in coin_list: # 작은 코인부터 시작해서 담아 나갈거기 떄문에 (왜냐하면 간격이 더 좁기 때문에)
