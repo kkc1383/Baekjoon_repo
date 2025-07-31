804201751912077a12e66205f43ae8f62b194347
kkc1383 <kangkc09@gmail.com>
2025-07-31 13:55:56 +0900

    solve prob_1388_s4.py

--- Silver/Silver 4/prob_1388_s4.py
+++ Silver/Silver 4/prob_1388_s4.py
@@ -1,24 +1,31 @@
-from collections import defaultdict
+from collections import deque
 import sys
 
-n=int(sys.stdin.readline().strip())
-num_list=list(map(int,sys.stdin.readline().strip().split()))
-answer_num=int(sys.stdin.readline().strip())
-answer_list=list(map(int,sys.stdin.readline().strip().split()))
+y_num,x_num=list(map(int,sys.stdin.readline().strip().split()))
 
-num_dict=defaultdict(int) # dict 초기화
+room_map=[list(sys.stdin.readline().strip()) for _ in range(y_num)]
 
-for num in num_list: # key : 배열의 숫자, value= 배열의 숫자가 나온 횟수
-    num_dict[num]+=1
+answer=0
+visited=[[False]*x_num for _ in range(y_num)]
+for y in range(y_num):
+    for x in range(x_num):
+        if visited[y][x]:
+            continue
+        
+        answer+=1
 
-result_list=[] # 해답을 저장하기 위한 공간
-for answer in answer_list:
-    if num_dict[answer]>0: # 원래 배열에 그 숫자가 있다면 0보다 클 것이기 때문에
-        result_list.append(1)
-    else: # 원래 배열에 그 숫자가 없다면
-        result_list.append(0)
-
-print(*result_list,sep=" ") # list의 [ ] 를 떼고 각 원소를 스페이스 한칸으로 구분하는 출력방법입니다.
-
-# 배열을 하고 이분탐색을 통해서 푸는 방법도 있다고 생각하지만, 배열의 크기가 커질수록 시간이 길어지기에 O(n)으로 끝낼 수 있는 해시테이블로 해결했습니다.
-# 
+        tp= 1 if room_map[y][x]=="-" else 0 # tp는 타일의 종류이고 -이면 1 |이면 0
+        myqueue=deque()
+        myqueue.append((y,x,tp))
+        
+        while myqueue:
+            top_y,top_x,top_tp=myqueue.popleft()
+            if top_tp==0: # 세로이면:
+                if top_y+1<y_num and not visited[top_y+1][top_x] and room_map[top_y+1][top_x]=="|": # 범위를 벗어나지 않고, 방문을 안 했고, 세로여야함
+                    myqueue.append((top_y+1,top_x,top_tp))
+                    visited[top_y+1][top_x]=True
+            else:
+                if top_x+1<x_num and not visited[top_y][top_x+1] and room_map[top_y][top_x+1]=="-": # 범위를 벗어나지 않고, 방문을 안 했고, 세로여야함
+                    myqueue.append((top_y,top_x+1,top_tp))
+                    visited[top_y][top_x+1]=True
+print(answer)
