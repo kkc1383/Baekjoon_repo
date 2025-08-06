b1067fc86f3bc4c8171133ae603d39f7058e3b2e
kkc1383 <kangkc09@gmail.com>
2025-08-07 00:45:45 +0900

    solve prob_14501_s3

---
+++ Silver/Silver 3/prob_14501_s3.py
@@ -0,0 +1,15 @@
+import sys
+
+finish=int(sys.stdin.readline().strip())
+
+meeting_list=[tuple(map(int,sys.stdin.readline().strip().split())) for _ in range(finish)]
+
+dp=[0]*30
+
+for i,(duration,reward) in enumerate(meeting_list):
+    dp[i]=max(dp[i],dp[i-1]) # 상담을 하지 않았을 수도 있기 때문에
+
+    if i+duration<=finish:
+        dp[i+duration]=max(dp[i+duration],dp[i]+reward)
+
+print(max(dp[:finish+1]))
