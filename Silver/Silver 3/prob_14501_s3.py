f1cc67c49d5a20d292b04b6c4afd4f7f59a8db48
kkc1383 <kangkc09@gmail.com>
2025-08-07 01:04:11 +0900

    refactor

--- Silver/Silver 3/prob_14501_s3.py
+++ Silver/Silver 3/prob_14501_s3.py
@@ -7,7 +7,7 @@ meeting_list=[tuple(map(int,sys.stdin.readline().strip().split())) for _ in rang
 dp=[0]*30
 
 for i,(duration,reward) in enumerate(meeting_list):
-    dp[i]=max(dp[i],dp[i-1]) # 상담을 하지 않았을 수도 있기 때문에
+    dp[i]=max(dp[i],dp[i-1]) # 상담을 하지 않았을 수도 있기 때문에 옆으로 확장을 해줘야 함
 
     if i+duration<=finish:
         dp[i+duration]=max(dp[i+duration],dp[i]+reward)
